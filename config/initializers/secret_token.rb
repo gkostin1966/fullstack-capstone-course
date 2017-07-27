d9588b12c656ff23be6ca6ebc21115dd0871861b
Greg Kostin <gkostin@umich.edu>
2017-07-27 12:04:17 -0400

    bundle exec rails-api new . -T -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Capstone::Application.config.secret_token = '5b312eda81758b1c979a005a60a2329b7677916173d0becfb2e634b750ba213eb60f82978decafbd7693828487a350b38bba037ff715fb2945a5ad9c4377b2d0'
