7a69448e722337579baf041bb2e3135ae76b7c75
Greg Kostin <gkostin@umich.edu>
2017-07-27 17:53:48 -0400

    RDBMS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :foos, except: [:new, :edit]
+  scope :api, defaults: {format: :json}  do
+    resources :foos, except: [:new, :edit]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
