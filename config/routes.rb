b41b04ead1bb5fd09e58f1f7d445a6004030cc83
Greg Kostin <gkostin@umich.edu>
2017-08-06 18:08:08 -0400

    Assignment: Module #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  scope :api, defaults: {format: :json} do
+    resources :states, except: [:new, :edit]
+    resources :cities, except: [:new, :edit]
+  end
+
   get '/ui' => 'ui#index'
   get '/*ui' => 'ui#index'
   root "ui#index"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
+City.find_or_create_by(name: 'Baltimore')
+State.find_or_create_by(name: 'Maryland')
