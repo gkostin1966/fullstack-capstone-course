71a6e835f802de2fe19f0d2903003820eff980e5
Greg Kostin <gkostin@umich.edu>
2017-08-06 17:02:27 -0400

    production

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-City.find_or_create_by(name: 'Baltimore')
-State.find_or_create_by(name: 'Maryland')
+
