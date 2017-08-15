ff2abcabf70dc2de3d8a00f16c062494f6dbb2d7
Greg Kostin <gkostin@umich.edu>
2017-08-15 15:46:30 -0400

    spec anatomy 2

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -2,28 +2,21 @@ require 'rails_helper'
 
 RSpec.describe City, type: :model do
   before(:all) { described_class.delete_all }
-  after(:each) { described_class.delete_all }
-
-  let(:name) { "name" }
+  after(:all) { described_class.delete_all }
 
   describe '#create' do
-    subject { described_class.create(name: name) }
-    it 'creates a described_class' do
-      expect(subject).to be_valid
-      expect(subject).to be_persisted
-      expect(subject.name).not_to be_nil
-      expect(subject.name).to eq(name)
-      expect(described_class.find(subject.id)).not_to be_nil
-      expect(described_class.find(subject.id)).to eq(subject)
-    end
+    subject { described_class.create }
+    it { is_expected.to be_valid }
+    it { is_expected.to be_persisted }
+    it { expect(described_class.find(subject.id)).not_to be_nil }
+    it { expect(described_class.find(subject.id)).to eq(subject) }
+    it { before_count = described_class.count; subject; expect(described_class.count).to eq(before_count + 1) }
   end
 
   describe '#name' do
-    subject { described_class.create(name: name) }
-    it 'has a name attribute' do
-      expect(subject).to be_valid
-      expect(subject.name).not_to be_nil
-      expect(subject.name).to eq(name)
-    end
+    let(:name) { "name" }
+    subject { described_class.create(name: name).name }
+    it { is_expected.not_to be_nil }
+    it { is_expected.to eq(name) }
   end
 end
