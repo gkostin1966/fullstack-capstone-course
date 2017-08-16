d3286c5b0505adda049341626d1bae7d5f297d93
Greg Kostin <gkostin@umich.edu>
2017-08-16 08:43:26 -0400

    city factories

--- spec/factories/cities.rb
+++ spec/factories/cities.rb
@@ -3,7 +3,35 @@ FactoryGirl.define do
     name "test"
   end
 
-  factory :city, parent: :city_fixed do
+  factory :city_sequence, class: 'City' do
+    sequence(:name) { |n| "test#{n}" }
+  end
 
+  factory :city_names, class: 'City' do
+    sequence(:name) { |n| ["larry", "moe", "curly"][n%3] }
+  end
+
+  factory :city_transient, class: 'City' do
+    transient do
+      male true
+    end
+
+    after(:build) do |object, props|
+      object.name = props.male ? "Mr Test" : "Ms Test"
+    end
+  end
+
+  factory :city_faker, class: 'City' do
+    name { Faker::Name.name }
+  end
+
+  factory :city_ctor, class: 'City' do
+    transient do
+      hash {}
+    end
+    initialize_with { City.new(hash) }
+  end
+
+  factory :city, parent: :city_fixed do
   end
 end
